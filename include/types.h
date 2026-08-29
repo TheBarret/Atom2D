91614fbdbf4a4683e5ece2e42b56ab2c09cade01
Zephyroku <format.e.drive@gmail.com>
2026-08-29 17:59:31 +0200

    Add types.h with vector, rectangle, color, and texture types

---
+++ include/types.h
@@ -0,0 +1,22 @@
+// types.h
+#ifndef TYPES_H
+#define TYPES_H
+#include <stdint.h>
+
+typedef struct { float x, y; } Vec2;
+typedef struct { float x, y, w, h; } Rect;
+typedef struct { uint8_t r, g, b, a; } Color;
+
+typedef struct {
+    void* handle;
+    int32_t width;
+    int32_t height;
+    uint8_t loaded;
+} Texture;
+
+Vec2 vec2(float x, float y);
+Rect rect(float x, float y, float w, float h);
+Color color(uint8_t r, uint8_t g, uint8_t b, uint8_t a);
+void texture_free(Texture* tex);
+
+#endif
