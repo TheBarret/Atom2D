91e0e4dd0ab61ceb9b16b48e6436fbfab67b8257
Zephyroku <format.e.drive@gmail.com>
2026-08-29 18:01:37 +0200

    Add types for Vec2, Rect, Color, and texture_free function

---
+++ src/types.c
@@ -0,0 +1,23 @@
+#include "../include/types.h"
+#include <stdlib.h>
+#include <stdbool.h>
+
+Vec2 vec2(float x, float y) {
+    return (Vec2){x, y};
+}
+
+Rect rect(float x, float y, float w, float h) {
+    return (Rect){x, y, w, h};
+}
+
+Color color(uint8_t r, uint8_t g, uint8_t b, uint8_t a) {
+    return (Color){r, g, b, a};
+}
+
+void texture_free(Texture* tex) {
+    if (tex && tex->loaded) {
+        // SDL will handle this later
+        tex->loaded = false;
+        tex->handle = 0;
+    }
+}
